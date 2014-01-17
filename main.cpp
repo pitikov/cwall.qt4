9e1d242b58434c0ada20d228c6d8d955216d195c
Pitikov Evgeniy <pitikov@yandex.ru>
2014-01-17 19:29:38 +0400

    Изменение вида заставки

--- main.cpp
+++ main.cpp
@@ -9,16 +9,17 @@ int main(int argc, char** argv)
 {
     QApplication app(argc, argv);
 		/* TODO change splash screen image
-		 * 
+		 *
 		 * Need use qt-resource file and theme-correct splash-screen image */
-		QImage img("/usr/share/wallpapers/Vector_Sunset/contents/screenshot.png");
+		QImage img("/usr/share/cwall/img/splash.jpg");
 		QSplashScreen splash(QPixmap::fromImage(img));
+		splash.showMessage("<h3><font color=#E20071>"+QApplication::tr("get auntefication")+"</font></h3>");
 		splash.show();
-		splash.showMessage(QApplication::tr("Get autenfication"));
-    CWallManager foo;
-		splash.showMessage(QApplication::tr("Prepare data"));
+    CWallManager *managerWin = new CWallManager;
+		splash.showMessage("<h3><font color=#E20071>"+QApplication::tr("load data")+"</font></h3>");
+		//splash.finish(managerWin);
 		Rules::sample();
-    foo.showMaximized();
+		managerWin->showMaximized();
 		splash.close();
     return app.exec();
 }
